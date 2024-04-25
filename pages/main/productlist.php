0f7d9570f64285d3aa3d820f126963f9d5853835
MukZro <anhngo876@gmail.com>
2024-04-25 11:05:25 +0700

    backupfile to fix all

--- pages/main/productlist.php
+++ pages/main/productlist.php
@@ -1,10 +1,26 @@
 <?php
-$sql_pro = "SELECT * FROM product WHERE product.category_id = '$_GET[id]' ORDER BY product.product_id DESC";
+
+
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+if ($page == '' || $page == 1) {
+    $begin = 0;
+} else {
+    $begin = ($page * 8) / $page;
+}
+
+
+$sql_pro = "SELECT * FROM product WHERE product.category_id = '$_GET[id]' ORDER BY product.product_id DESC LIMIT $begin,8";
 $query_pro = mysqli_query($mysqli, $sql_pro);
 // danhmuc
 $sql_cate = "SELECT * FROM category WHERE category_id = '$_GET[id]' LIMIT 1";
 $query_cate = mysqli_query($mysqli, $sql_cate);
 $row_title = mysqli_fetch_array($query_cate);
+
 ?>
 
 <h3 style="color: black; font-size:50px"><?php echo $row_title['category_name'] ?></h3>
@@ -40,12 +56,26 @@ $row_title = mysqli_fetch_array($query_cate);
         </span>
     </div> <!-- .filter -->
 
-    <div class="pagination">
-        <a href="#" class="page-number"><i class="fa fa-angle-left"></i></a>
-        <span class="page-number current">1</span>
-        <a href="#" class="page-number">2</a>
+    <?php
+    $category_id = $row_title['category_id'];
+    $sql_trang = "SELECT * FROM product WHERE product.category_id = $category_id ";
+    $query_trang = mysqli_query($mysqli, $sql_trang);
+    $row_count = mysqli_num_rows($query_trang);
+    $trang = ceil($row_count / 8);
+    ?>
 
-        <a href="#" class="page-number"><i class="fa fa-angle-right"></i></a>
+    <div class="pagination">
+
+        <a href="#" class="page-number"><i class="fa fa-angle-left"></i></a>
+        <?php
+        echo $page;
+        for ($i = 1; $i <= $trang; $i++) {
+            $isActive = ($i == $page) ? 'background-color: rgb(80,188,133);' : '';
+        ?>
+            <a href="index.php?manage=product&page=<?php echo $i ?>&id=<?php echo $row_title['category_id'] ?>" class="page-number" style="color:black; margin-left: 10px;  <?php echo $isActive; ?>"><?php echo $i ?></a>
+        <?php } ?>
+        <a href="#" class="page-number" style="margin-left: 10px;"><i class="fa fa-angle-right"></i></a>
+
     </div> <!-- .pagination -->
 </div> <!-- .filter-bar -->
 
@@ -70,11 +100,18 @@ $row_title = mysqli_fetch_array($query_cate);
 </div> <!-- .product-list -->
 
 <div class="pagination-bar">
-    <div class="pagination ">
-        <a href="#" class="page-number"><i class="fa fa-angle-left"></i></a>
-        <span class="page-number current">1</span>
-        <a href="#" class="page-number">2</a>
+    <div class="pagination">
 
-        <a href="#" class="page-number"><i class="fa fa-angle-right"></i></a>
+        <a href="#" class="page-number"><i class="fa fa-angle-left"></i></a>
+        <?php
+        echo $page;
+        for ($i = 1; $i <= $trang; $i++) {
+            $isActive = ($i == $page) ? 'background-color: rgb(80,188,133);' : '';
+        ?>
+            <a href="index.php?manage=product&page=<?php echo $i ?>&id=<?php echo $row_title['category_id'] ?>" class="page-number" style="color:black; margin-left: 10px;  <?php echo $isActive; ?>"><?php echo $i ?></a>
+        <?php } ?>
+        <a href="#" class="page-number" style="margin-left: 10px;"><i class="fa fa-angle-right"></i></a>
+
     </div> <!-- .pagination -->
+    <p style=" color:black;">Current page: <?php echo $page ?>/<?php echo $trang ?></p>
 </div>
