a853fc960db1a173045f6382c1b3ce54cad488aa
MukZro <anhngo876@gmail.com>
2024-05-10 17:47:31 +0700

    finish

--- admincp/modules/header.php
+++ admincp/modules/header.php
@@ -15,7 +15,7 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="#">
-                <img src="assets/img/logo.png" />
+                
             </a>
 
         </div>
--- admincp/modules/manageorder/vieworder.php
+++ admincp/modules/manageorder/vieworder.php
@@ -1,11 +1,26 @@
 <p>View detail orders</p>
 <?php
+
+// Fetch shipping address
+$sql_shipping_address = "SELECT * FROM tbl_shipping WHERE code_orders = '$_GET[coder]'";
+$query_shipping_address = mysqli_query($mysqli, $sql_shipping_address);
+$row_shipping_address = mysqli_fetch_assoc($query_shipping_address);
+$customer_name = $row_shipping_address['name'];
+$shipping_province = $row_shipping_address['province'];
+$shipping_district = $row_shipping_address['district'];
+$shipping_ward = $row_shipping_address['ward'];
+$shipping_address = $row_shipping_address['address'];
+
+// Fetch order details
+
 $sql_lietke_dh = "SELECT * FROM order_details,product WHERE order_details.product_id = product.product_id AND order_details.code_orders = '$_GET[coder]' ORDER BY order_details.id_order_details DESC";
 $query_lietke_dh = mysqli_query($mysqli, $sql_lietke_dh);
 $total_money = 0;
 ?>
 
 <h3>Id order: <?php echo $_GET['coder'] ?> </h3>
+<h3>Customer Name: <?php echo $customer_name ?> </h3>
+<h3 style="margin-bottom: 30px;">Shipping Address:  <span><?php echo $shipping_address; ?>, <?php echo $shipping_ward; ?>, <?php echo $shipping_district; ?>, <?php echo $shipping_province; ?></span></h3>
 <table class="table">
     <tbody>
         <tr>
